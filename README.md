a4998c724ca8029be2e51c375abe1768072fd6a2
Tony Rizzotto <anthony.rizzotto@zenbusiness.com>
2022-01-11 08:47:19 -0500

    Update logic to include filtering and update README

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ## About this function
 This function can be run to merge two different JSON documents based on a common field. If the common field is missing from any object in the 2nd data array, the function will not run because we are looking for a 1-1 relationship with the common field. 
+
+To bypass this functionality, you can include the `if/else` statement in line 20 to check for your desired fields and return only what you need. This example only returns the original object, but you can include any piece of information you need. 
--- index.js
+++ index.js
@@ -11,23 +11,33 @@ const mergeJSONByField = (arr1, arr2) => {
   // Map through the first array
   res = arr1.map((obj) => {
     // Get the index of synced fields
-    const index = arr2.findIndex(el => el["email"] === obj["email"])
+    const index = arr2.findIndex(el => el["email"] === obj["email"]);
 
-    // Extract the desired fields from the 2nd JSON object: can also rename through destructuring.
-    // This will not depend on the data's organization, but rather the unique field defined above.
-    const { first_name: firstName, last_name: lastName } = arr2[index];
+    // To scan an object for compatability before extracting the fields we need:
+    const dataFields = arr2[index];
 
-    // Spread the original oobject and return with updated fields
-    return {
-      ...obj,
-      firstName,
-      lastName
-    }
-  })
+    // These fields can be updated to any field values you need. Return only what you want.
+    if (!dataFields.email || !dataFields.last_name){
+      return {
+        ...obj
+      };
+    } else {
+      // Extract the desired fields from the 2nd JSON object: can also rename through destructuring.
+      // This will not depend on the data's organization, but rather the unique field defined above.
+      const { first_name: firstName, last_name: lastName } = arr2[index];
+
+      // Spread the original oobject and return with updated fields
+      return {
+        ...obj,
+        firstName,
+        lastName
+      };
+    };
+  });
 
   // Compile the entire res array into a new JSON document
-  fs.writeFileSync('./data3.json', JSON.stringify(res))
+  fs.writeFileSync('./data3.json', JSON.stringify(res));
 }
 
 // Execute the function
-mergeJSONByField(data1, data2)
+mergeJSONByField(data1, data2);
